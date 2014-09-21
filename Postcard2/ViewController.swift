02c438ac6e8dfd6dea7f839326dfb7519b798eb9
Tim <tim@agilitee.com>
2014-09-21 12:42:20 -0400

    initial add to postcard2

--- Postcard2/ViewController.swift
+++ Postcard2/ViewController.swift
@@ -3,13 +3,21 @@
 //  Postcard2
 //
 //  Created by Tim Weaver on 9/20/14.
-//  Copyright (c) 2014 Buring Ember. All rights reserved.
+//  Copyright (c) 2014 Burning Ember. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterMessageText: UITextField!
+    
+    @IBOutlet weak var enterNameText: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPress(sender: UIButton) {
+        //code will evaluate when we press the button
+        messageLabel.hidden=false
+        messageLabel.text = enterMessageText.text
+        enterMessageText.text=""
+        enterMessageText.resignFirstResponder();
+        messageLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 
 }
 
